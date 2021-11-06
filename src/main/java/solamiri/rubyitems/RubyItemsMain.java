8815b98be7d2fdaa7c7c88d685abda74621a622b
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2021-11-06 19:19:30 -0400

    Add ruby
    Add Item, Lang, and Png

--- src/main/java/solamiri/rubyitems/RubyItemsMain.java
+++ src/main/java/solamiri/rubyitems/RubyItemsMain.java
@@ -3,12 +3,14 @@ package solamiri.rubyitems;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import solamiri.rubyitems.item.RubyItems;
 
 public class RubyItemsMain implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("rubyitems");
+	public static final String MOD_ID = "rubyitems";
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +19,8 @@ public class RubyItemsMain implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		RubyItems.registerRubyItemItems();
+
 	}
 }
---
+++ src/main/java/solamiri/rubyitems/item/RubyItems.java
@@ -0,0 +1,24 @@
+package solamiri.rubyitems.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import solamiri.rubyitems.RubyItemsMain;
+
+public class RubyItems{
+ 
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings().group(RubyItemGroup.RUBY)));
+  
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(RubyItemsMain.MOD_ID,name),item);
+    }
+
+
+    public static void registerRubyItemItems(){
+        System.out.println("Registering RubyItem Items for " + RubyItemsMain.MOD_ID);
+    }
+
+    
+
+}
