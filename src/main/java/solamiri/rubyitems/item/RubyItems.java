76e50bdd0b81e4b4823374ffcfa099d49ecda65f
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2021-11-07 15:01:08 -0500

    Ajout Ruby Royal Armor
    Ajout de l’armure Ruby Royal, 4 items, 4 recettes, models textures et item textures

--- src/main/java/solamiri/rubyitems/item/RubyItems.java
+++ src/main/java/solamiri/rubyitems/item/RubyItems.java
@@ -1,6 +1,8 @@
 package solamiri.rubyitems.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -8,8 +10,29 @@ import solamiri.rubyitems.RubyItemsMain;
 
 public class RubyItems{
  
-    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings().group(RubyItemsGroup.RUBY)));
+    //Add the ruby item
+    public static final Item RUBY = registerItem("ruby", 
+    new Item(new FabricItemSettings().group(RubyItemsGroup.RUBY)));
  
+    //Add the ruby royal armor
+    // slightly better than gold armor
+    public static final Item RUBY_ROYAL_CROWN = registerItem("ruby_royal_crown", 
+    new ArmorItem(RubyItemsArmorMaterial.RUBY,EquipmentSlot.HEAD,
+    new FabricItemSettings().group(RubyItemsGroup.RUBY)));
+
+    public static final Item RUBY_ROYAL_CHESTPLATE = registerItem("ruby_royal_chestplate", 
+    new ArmorItem(RubyItemsArmorMaterial.RUBY,EquipmentSlot.CHEST,
+    new FabricItemSettings().group(RubyItemsGroup.RUBY)));
+
+    public static final Item RUBY_ROYAL_LEGS = registerItem("ruby_royal_legs", 
+    new ArmorItem(RubyItemsArmorMaterial.RUBY,EquipmentSlot.LEGS,
+    new FabricItemSettings().group(RubyItemsGroup.RUBY)));
+
+    public static final Item RUBY_ROYAL_BOOTS = registerItem("ruby_royal_boots", 
+    new ArmorItem(RubyItemsArmorMaterial.RUBY,EquipmentSlot.FEET,
+    new FabricItemSettings().group(RubyItemsGroup.RUBY)));
+
+
     /**
      * Register function for items
      * @param name  The name of the item
